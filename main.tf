f211ae0c5053a463afb4c49c7db60397fbab3931
jobinmpz <122361325+jobinmpz@users.noreply.github.com>
2024-11-27 13:06:45 +0530

    Security Group Create update 2

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@
 		from_port	=	80
 		to_port		=	80
 		protocol	=	"tcp"
-		cidr_bocks	=	["0.0.0.0/0"]
+		cidr_blocks	=	["0.0.0.0/0"]
 		
 		security_group_id	=	aws_security_group.blog.id
 		}	
@@ -50,7 +50,7 @@
 		from_port	=	443
 		to_port		=	443
 		protocol	=	"tcp"
-		cidr_bocks	=	["0.0.0.0/0"]
+		cidr_blocks	=	["0.0.0.0/0"]
 		
 		security_group_id	=	aws_security_group.blog.id
 		}	
@@ -60,7 +60,7 @@
 		from_port	=	0
 		to_port		=	0
 		protocol	=	"-1"
-		cidr_bocks	=	["0.0.0.0/0"]
+		cidr_blocks	=	["0.0.0.0/0"]
 		
 		security_group_id	=	aws_security_group.blog.id
 		}	
