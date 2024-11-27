f4b686f7fdd322ea8a149b0d893b8764b56083ab
jobinmpz <122361325+jobinmpz@users.noreply.github.com>
2024-11-27 13:03:26 +0530

    Craate Security Group

--- main.tf
+++ main.tf
@@ -1,24 +1,66 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+	data	"aws_ami"	"app_ami"{
+		most_recent	=	true
+		
+		filter{
+			name	=	"name"
+			values	=	["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+		}
+		
+		filter{
+			name	=	"virtualization-type"
+			values	=	["hvm"]
+		}
+		
+		owners	=	["637423663107"] # bitnami
+		
+	data	"aws_vpc"	"default"{
+		default	=	true
+		}
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
+	resource "aws_instance" "blog"{
+		ami				=	data.aws_ami.app_ami.id
+		instance_type	=	var.instance_type
+		
+		vpc_security_group_ids	=	[aws_security_group.blog.id]
+		
+		tags	=	{
+			Name	=	"Learming-Terraform"
+			}
+		}
+		
+	resource "aws_security_group" "blog"{
+		name		=	"blog"
+		description	=	"Allow http and https in. Allow everything Out"
+		
+		vpc_id	=	data.aws_vpc.default.id
+		}
+		
+	resource	"aws_security_group_rule" "blog_http_in"{
+		type		=	"ingress"
+		from_port	=	80
+		to_port		=	80
+		protocol	=	"tcp"
+		cidr_bocks	=	["0.0.0.0/0"]
+		
+		security_group_id	=	aws_security_group.blog.id
+		}	
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+	resource	"aws_security_group_rule" "blog_https_in"{
+		type		=	"ingress"
+		from_port	=	443
+		to_port		=	443
+		protocol	=	"tcp"
+		cidr_bocks	=	["0.0.0.0/0"]
+		
+		security_group_id	=	aws_security_group.blog.id
+		}	
+		
+	resource	"aws_security_group_rule" "blog_everything_out"{
+		type		=	"egress"
+		from_port	=	0
+		to_port		=	0
+		protocol	=	"-1"
+		cidr_bocks	=	["0.0.0.0/0"]
+		
+		security_group_id	=	aws_security_group.blog.id
+		}	
